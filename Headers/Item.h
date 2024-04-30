5611f16a29d6cb147fd8c1391418106aa2e79968
sloggo <70816908+sloggo@users.noreply.github.com>
2024-04-30 17:12:45 +0100

    Add item header

---
+++ Headers/Item.h
@@ -0,0 +1,19 @@
+//
+// Created by Josh Sloggett on 30/04/2024.
+//
+
+#ifndef UNTITLED2_ITEM_H
+#define UNTITLED2_ITEM_H
+#include<string>
+
+using namespace std;
+
+class Item{
+protected:
+    string name;
+    string description;
+public:
+    void use();
+};
+
+#endif //UNTITLED2_ITEM_H
