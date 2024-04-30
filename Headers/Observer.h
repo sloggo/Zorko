8d9d14d4a548d5f1ffe9791cafe4b83843a3adcc
sloggo <70816908+sloggo@users.noreply.github.com>
2024-04-30 17:11:10 +0100

    Add observer header

---
+++ Headers/Observer.h
@@ -0,0 +1,15 @@
+//
+// Created by Josh Sloggett on 30/04/2024.
+//
+
+#ifndef UNTITLED2_OBSERVER_H
+#define UNTITLED2_OBSERVER_H
+
+using namespace std;
+
+class Observer{
+public:
+    void update(int,int); // for updating location
+};
+
+#endif //UNTITLED2_OBSERVER_H
